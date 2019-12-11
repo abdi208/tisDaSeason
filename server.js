e81a23ae169f51dd7c5c883dd83f63ef15a94e75
Ha-g <you@example.com>
2019-12-11 10:50:48 -0800

    static changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded( { extended: false }));
 app.use(express.json());
-app.use(express.static('static'))
+app.use(express.static(__dirname + '/client/build'))
 app.use(helmet())
 app.use(cors())
 
