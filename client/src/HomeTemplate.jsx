876d7f25a1b607153c22d4f6d2fbcd43fdf0180a
Ha-g <you@example.com>
2019-12-10 20:23:21 -0800

    more pro looking

--- client/src/HomeTemplate.jsx
+++ client/src/HomeTemplate.jsx
@@ -15,20 +15,20 @@ export default function HomeTemplate() {
 
   return (
     <Paper className={classes.root} style={{background: 'none', height: '100vh' }}>
-      <Typography style={{fontSize: '50px', color: 'black', display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
+      <Typography style={{fontSize: '50px', color: 'green',fontFamily: 'Mountains of Christmas', display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
       'tis
       </Typography>
-      <Typography style={{fontSize: '50px', color: 'blue', display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
+      <Typography style={{fontSize: '75px', fontFamily: 'Mountains of Christmas', color: 'blue', display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
       Da
       </Typography>
-      <Typography style={{fontSize: '50px', color: 'red',  display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
+      <Typography style={{fontSize: '75px', fontFamily: 'Mountains of Christmas', color: 'red',  display: 'inline-block', marginTop: '39vh' }} variant="h5" component="h3">
       Season 
       </Typography>
       <Typography component="p">
         
       </Typography>
-      <Button style={{fontSize: '50px', color: 'red', display: 'inline-block', marginTop: '5vh' }}><Link style={{color: 'blue', textDecoration: 'none'}} to='/login'>Login</Link></Button>
-            <Button style={{fontSize: '50px', color: 'blue',  display: 'inline-block', marginTop: '5vh' }} ><Link style={{color: 'black', textDecoration: 'none'}}to='/signup'>Signup</Link></Button>
+      <Button style={{fontSize: '50px', fontFamily: 'Mountains of Christmas', color: 'red', display: 'inline-block', marginTop: '5vh' }}><Link style={{color: 'blue', textDecoration: 'none'}} to='/login'>Login</Link></Button>
+            <Button style={{fontSize: '50px', color: 'blue', fontFamily: 'Mountains of Christmas', display: 'inline-block', marginTop: '5vh' }} ><Link style={{color: 'green', textDecoration: 'none'}}to='/signup'>Signup</Link></Button>
     </Paper>
   );
 }
